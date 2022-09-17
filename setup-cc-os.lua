234f30f09c9459269d4b7a20587acea2cdeff04a
Eric Sexton <foopis23@gmail.com>
2022-09-17 00:17:41 -0400

    added shedit to os by default

--- setup-cc-os.lua
+++ setup-cc-os.lua
@@ -1,5 +1,7 @@
 -- This is a one time run script to setup the CC OS to be more linux like
 local startup_script_url = "https://raw.githubusercontent.com/foopis23/cc-baco/master/startup-cc-os.lua"
+local lex_library_url = "https://raw.githubusercontent.com/foopis23/cc-baco/master/lex.lua"
+local shedit_url = "https://raw.githubusercontent.com/foopis23/cc-baco/master/shedit.lua"
 
 textutils.slowPrint("Setting up...")
 
@@ -9,15 +11,20 @@ fs.makeDir("/etc")
 fs.makeDir("/home")
 fs.makeDir("/lib")
 
--- download startup script
-local request = http.get(startup_script_url)
-local startup = request.readAll()
-request.close()
+local function downloadFile(url, path)
+	local request = http.get(url)
+	local startup = request.readAll()
+	request.close()
 
--- save startup script
-local fd = fs.open("startup", "w")
-fd.write(startup)
-fd.close()
+	local fd = fs.open(path, "w")
+	fd.write(startup)
+	fd.close()
+end
+
+-- download startup script
+downloadFile(startup_script_url, "/startup")
+downloadFile(lex_library_url, "/lib/lex.lua")
+downloadFile(shedit_url, "/bin/shedit")
 
 -- reboot system
 textutils.slowPrint("Setup Complete. Restarting...")
